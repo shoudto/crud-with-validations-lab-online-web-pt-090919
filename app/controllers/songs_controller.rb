c300937a13667993a7b6d1de584d2cf0131e1c91
Oscar Shoudt <shoudto@gmail.com>
2020-07-14 13:34:53 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,48 +1,49 @@
 class SongsController < ApplicationController
-    before_action :set_song, only:[:show, :edit, :update, :destroy]
+    before_action :set_song, only:[:show, :edit, :update, :destroy] # To be able to use the strong params
 
-    def index
-        @songs = Song.all
+    def index # implicit rendering
+        @songs = Song.all # List all the songs 
     end
 
     def new
-        @song = Song.new 
+        @song = Song.new # create a new song
     end
 
     def show
     end
 
-    def create
-        @song = Song.new(song_params)
+    def create # implicit rendering 
+        @song = Song.new(song_params) # create a new song
 
-        if @song.valid?
+        if @song.valid? # check is the song is valid with the correct attributes
             @song.save
             redirect_to song_path(@song)
         else
-            render :new 
+            render :new # explicit rendering 
         end 
     end 
 
     def edit
     end 
 
-    def update
-        if @song.update(song_params)
+    def update # implicit rendering
+        if @song.update(song_params) # update the song parameters
             redirect_to song_path(@song)
         else
-            render :edit
+            render :edit # explicit rendering
         end
     end
 
-    def destroy
+    def destroy # implicit rendering
         @song.destroy
-        redirect_to songs_path
+        redirect_to songs_path # explicit rendering
     end 
 
     private 
 
+    # strong params
     def set_song
-        @song = Song.find(params[:id])
+        @song = Song.find(params[:id]) 
     end
 
     def song_params
